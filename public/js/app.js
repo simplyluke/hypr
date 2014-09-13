ab7d80b023c824035bae2aa3cfdd72f75f888cfa
Luke Wright <lukewright1997@gmail.com>
2014-09-13 13:23:29 -0400

    working commit

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,12 @@ var hypr = angular.module('hypr', ['google-maps']);
 
 
 hypr.controller('HyprCtrl', function($scope) {
+  // default the scope lat and long
   $scope.lat = "0";
   $scope.lng = "0";
   $scope.markers = [];
 
-
+  // get user geolocation - yay html5
   $scope.getLocation = function () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position){
@@ -17,17 +18,12 @@ hypr.controller('HyprCtrl', function($scope) {
      console.log("Geolocation is not supported by this browser.");
     }
   };
-
+  // fire the get event
   $scope.getLocation();
 
-  $scope.map = {
-    center: {
-      latitude: 0,
-      longitude: 0
-    },
-    zoom: 12
-  };
-
+  // onSuccess is the callback for the geolocation get 
+  // this function executes upon successful retrieval of the user's location
+  // updates the map scope
   var onSuccess = function(position) {
     console.log(position.coords.latitude + ' ' + position.coords.longitude);
     $scope.map.center = {
@@ -38,4 +34,54 @@ hypr.controller('HyprCtrl', function($scope) {
 
     $scope.$apply();
   };
+
+  // map style
+
+  $scope.map.style = styleArray = [
+  {
+    featureType: "all",
+    stylers: [
+      { saturation: -80 }
+    ]
+  },{
+    featureType: "road.arterial",
+    // elementType: "geometry",
+    stylers: [
+      { hue: "#00ffee" },
+      { saturation: 50 }
+    ]
+  },{
+    featureType: "poi.business",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+
+  // set up the map parts of the scope
+  $scope.map = {
+    center: {
+      // default to Upenn
+      latitude: 39.951715199999995,
+      longitude: -75.1915001
+    },
+    zoom: 12,
+    // custom google maps options
+    options: {
+      // disable a shit ton of map features
+      zoomControl: false,
+      // minZoom *should* be redundant but I'd rather not let them zoom out to the world view
+      minZoom: 11,
+      scrollwheel: false,
+      panControl: false,
+      rotateControl: false,
+      scaleControl: false,
+      streetViewControl: false
+    }
+  };
+
+
+
 });
