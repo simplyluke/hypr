0bf8447cd1c02aa2aeccd4dd0f29082091e5133a
Luke Wright <lukewright1997@gmail.com>
2014-09-13 02:00:53 -0500

    Holy shit I think it works

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,41 @@
-angular.module('hypr', ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider.when('/', {
-      controller: 'HyprCtrl',
-      templateUrl: 'hypr-index.html'
-    }).otherwise({
-      redirectTo: '/'
-    });
-  });
+var hypr = angular.module('hypr', ['google-maps']);
+
+
+hypr.controller('HyprCtrl', function($scope) {
+  $scope.lat = "0";
+  $scope.lng = "0";
+  $scope.markers = [];
+
+
+  $scope.getLocation = function () {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        onSuccess(position);
+      });
+    }
+    else {
+     console.log("Geolocation is not supported by this browser.");
+    }
+  };
+
+  $scope.getLocation();
+
+  $scope.map = {
+    center: {
+      latitude: 0,
+      longitude: 0
+    },
+    zoom: 12
+  };
+
+  var onSuccess = function(position) {
+    console.log(position.coords.latitude + ' ' + position.coords.longitude);
+    $scope.map.center = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    };
+
+
+    $scope.$apply();
+  };
+});
